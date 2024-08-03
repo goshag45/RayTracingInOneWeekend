5e1b69791c815afb3e5ad5264ce9786b3f519cae
goshag45 <goshagugl44@gmail.com>
2024-08-03 22:27:49 +1000

    begin 7

---
+++ camera.h
@@ -0,0 +1,28 @@
+#ifndef CAMERA_H
+#define CAMERA_H
+
+#include "rtweekend.h"
+
+#include "hittable.h"
+
+class camera {
+  public:
+    /* Public Camera Parameters Here */
+
+    void render(const hittable& world) {
+        ...
+    }
+
+  private:
+    /* Private Camera Variables Here */
+
+    void initialize() {
+        ...
+    }
+
+    color ray_color(const ray& r, const hittable& world) const {
+        ...
+    }
+};
+
+#endif
