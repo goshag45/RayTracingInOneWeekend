d7482f42e77bc8fdc4de66cf5e15d04ab5a2b39e
goshag45 <goshagugl44@gmail.com>
2024-08-05 15:42:08 +1000

    8.2 + infinity not working again

--- interval.h
+++ interval.h
@@ -1,6 +1,7 @@
 #ifndef INTERVAL_H
 #define INTERVAL_H
 
+
 class interval {
   public:
     double min, max;
@@ -19,6 +20,12 @@ class interval {
 
     bool surrounds(double x) const {
         return min < x && x < max;
+    }
+
+    double clamp(double x) const {
+        if (x < min) return min;
+        if (x > max) return max;
+        return x;
     }
 
     static const interval empty, universe;
